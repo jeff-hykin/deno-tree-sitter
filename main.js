a5af7e44b574ea086214552782680bde94a32003
Jeff Hykin <jeff.hykin@gmail.com>
2024-07-03 17:00:52 -0500

    add xmlStylePreview

--- main.js
+++ main.js
@@ -302,4 +302,83 @@ export const applyThemeGetHtml = ({ themeRules, tree, string }) => {
     }
     innerHtml += `<p>${escapeHtml(string.slice(prevSlicePoint))}</p>`
     return innerHtml;
-};
+}
+
+
+const quoteIfNeeded = (word)=>{
+    if (word.match(/[a-zA-Z0-9]+/)) {
+        return word
+    } else {
+        return JSON.stringify(word)
+    }
+}
+
+/**
+ * helpful for visualizing the AST
+ *
+ * @example
+ * ```js
+ * const tree = parser.parse({
+ *     string: `
+ *         function thing(arg1) {
+ *             let a = 10
+ *         }
+ *     `,
+ *     withWhitespace: false,
+ * })
+ * console.log(xmlStylePreview(tree.rootNode))
+ * // <program>
+ * //     <function_declaration>
+ * //         <function text="function" />
+ * //         <identifier text="thing" />
+ * //         <formal_parameters>
+ * //             <"(" text="(" />
+ * //             <identifier text="arg1" />
+ * //             <")" text=")" />
+ * //         </formal_parameters>
+ * //         <statement_block>
+ * //             <"{" text="{" />
+ * //             <lexical_declaration>
+ * //                 <let text="let" />
+ * //                 <variable_declarator>
+ * //                     <identifier text="a" />
+ * //                     <"=" text="=" />
+ * //                     <number text="10" />
+ * //                 </variable_declarator>
+ * //             </lexical_declaration>
+ * //             <"}" text="}" />
+ * //         </statement_block>
+ * //     </function_declaration>
+ * // </program>
+ * ```
+ *
+ * @param {Node} startNode - 
+ * @param {Object} options - 
+ * @param {Boolean} options.alwaysShowTextAttr - parent nodes will get a text attribute (NOTE: it can be massive for large trees)
+ * @returns {String} output - html-like formatted string of the AST
+ */
+export function xmlStylePreview(startNode, { alwaysShowTextAttr = false } = {}) {
+    // 
+    // usage
+    // 
+    let output = ""
+    let indent = ""
+    for (const [ parents, node, direction ] of startNode.traverse()) {
+        if (direction == "-") {
+            output += indent+`<${quoteIfNeeded(node.type)} text=${JSON.stringify(node.text)} />\n`
+        } else {
+            if (direction == "->") {
+                if (alwaysShowTextAttr) {
+                    output += indent+`<${quoteIfNeeded(node.type)} text=${JSON.stringify(node.text)} />\n`
+                } else {
+                    output += indent+`<${quoteIfNeeded(node.type)}>\n`
+                }
+                indent += "    "
+            } else if (direction == "<-") {
+                indent = indent.slice(0,-4)
+                output += indent+`</${quoteIfNeeded(node.type)}>\n`
+            }
+        }
+    }
+    return output
+}
--- tests/traverse_1.js
+++ tests/traverse_1.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S deno run --allow-all
-import { Parser, parserFromWasm } from "../main.js"
+import { Parser, parserFromWasm, xmlStylePreview } from "../main.js"
 import javascript from "https://github.com/jeff-hykin/common_tree_sitter_languages/raw/4d8a6d34d7f6263ff570f333cdcf5ded6be89e3d/main/javascript.js"
+
 const parser = await parserFromWasm(javascript) // path or Uint8Array
 var tree = parser.parse({
     string: `
@@ -10,45 +11,33 @@ var tree = parser.parse({
     `,
     withWhitespace: false,
 })
+console.log(xmlStylePreview(tree.rootNode))
+console.log(xmlStylePreview(tree.rootNode, { alwaysShowTextAttr: true }))
 
-// 
-// usage
-// 
-let indent = ""
-for (const [ parents, node, direction ] of tree.rootNode.traverse()) {
-    if (direction == "-") {
-        console.log(indent+`<${node.type} text=${JSON.stringify(node.text)} />`)
-    } else {
-        if (direction == "->") {
-            console.log(indent+`<${node.type}>`)
-            indent += "    "
-        } else if (direction == "<-") {
-            indent = indent.slice(0,-4)
-            console.log(indent+`</${node.type}>`)
-        }
-    }
-}
+// // 
+// // usage
+// // 
 
-console.log(``)
-var tree = parser.parse({
-    string: `
-        function thing(arg1, arg2) {
-            let a = 10
-        }
-    `,
-    withWhitespace: false,
-})
-indent = ""
-for (const [ parents, node, direction ] of tree.rootNode.traverse()) {
-    if (direction == "-") {
-        console.log(indent+`${JSON.stringify(node.type)}: ${JSON.stringify(node.text)}`)
-    } else {
-        if (direction == "->") {
-            console.log(indent+`<${node.type}>`)
-            indent += "    "
-        } else if (direction == "<-") {
-            indent = indent.slice(0,-4)
-            console.log(indent+`</${node.type}>`)
-        }
-    }
-}
+// console.log(``)
+// var tree = parser.parse({
+//     string: `
+//         function thing(arg1, arg2) {
+//             let a = 10
+//         }
+//     `,
+//     withWhitespace: false,
+// })
+// let indent = ""
+// for (const [ parents, node, direction ] of tree.rootNode.traverse()) {
+//     if (direction == "-") {
+//         console.log(indent+`${JSON.stringify(safeEscape(node.type))}: ${JSON.stringify(node.text)}`)
+//     } else {
+//         if (direction == "->") {
+//             console.log(indent+`<${safeEscape(node.type)}>`)
+//             indent += "    "
+//         } else if (direction == "<-") {
+//             indent = indent.slice(0,-4)
+//             console.log(indent+`</${safeEscape(node.type)}>`)
+//         }
+//     }
+// }
