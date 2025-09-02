d411c5fcc74789e656d38253bcfdebf257464ed9
Jeff Hykin <jeff.hykin@gmail.com>
2025-09-02 14:56:09 -0500

    fix finding names

--- main/extras/misc.js
+++ main/extras/misc.js
@@ -7,6 +7,6 @@ export function _getQueryCaptureTargets(queryString) {
         const safeSearchableQuery = queryString.replace(/"(?:\\(?:[^xu0-7]|[0-7]{1,3}|x[0-9a-fA-F]{2}|u[0-9a-fA-F]{4}|u{[0-9a-fA-F]+})|[^\"\\])*"/g, "")
         // now check if there is a capture target
         // NOTE: also based on the language spec: https://github.com/tree-sitter-grammars/tree-sitter-query/blob/3a9808b22742d5bd906ef5d1a562f2f1ae57406d/src/grammar.json#L204
-        return safeSearchableQuery.match(/@([a-zA-Z0-9.\-_\$]+)/g)
+        return safeSearchableQuery.match(/(?<=@)([a-zA-Z0-9.\-_\$]+)/g)
     }
 }
